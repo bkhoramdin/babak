bd7d1d1de5c2e98719e515fc58d9b818afc37589
bkhoramdin <b_khoramdin@yahoo.com>
2015-03-24 19:54:51 +0800

    commit

---
+++ HelloWorldService/IRedPill.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.InteropServices;
+using System.ServiceModel.Web;
+using System.Runtime.Serialization;
+
+
+
+namespace MyWCFServices
+{
+  
+    [DataContract(Name="Triangle")]
+
+    public enum TriangleType
+    {
+        [EnumMember]
+        Equilateral,
+        [EnumMember]
+        Isosceles,
+       [EnumMember]
+        Scalene,
+        [EnumMember]
+
+        Error
+    }
+     [ServiceContract(Namespace= "http://KnockKnock.readify.net")]
+    public interface IRedPill
+    {
+         
+        [OperationContract]
+     
+        System.Guid WhatIsYourToken( );
+
+
+         [OperationContract]
+
+        TriangleType WhatShapeIsThis(int a, int b, int c);
+
+          [OperationContract]
+          [FaultContract(typeof(ArgumentOutOfRangeException))]
+         long FibonacciNumber(long n);
+          
+         
+         
+         [OperationContract]
+         [FaultContract(typeof(ArgumentNullException))]
+          string ReverseWords(string s);
+
+        
+
+    }
+}
---
+++ HelloWorldService/RedPill.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+
+namespace MyWCFServices
+
+
+
+{
+    public class RedPill : IRedPill
+    {
+
+       
+        public System.Guid WhatIsYourToken()
+        {
+            Guid g = new Guid("702cff1c-675d-4d98-92dc-6d88f9a7304e");
+
+            return g;
+
+        }
+        public TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                return TriangleType.Error;
+            else
+
+            if (a == b && b == c)
+
+                return TriangleType.Equilateral;
+            else if (a == b || b == c || a == c)
+                return TriangleType.Isosceles;
+            else if(a != b && b != c)
+                return TriangleType.Scalene;
+            else           
+                return TriangleType.Error;
+                        
+        }
+        public long FibonacciNumber(long x)
+        {
+           
+            if (x>92 || x<-92)
+            {
+                throw new ArgumentOutOfRangeException("Fib", "less than 92");
+            }
+
+            if (x < 0)
+                return 1;
+            
+            if (x == 0) return 0;
+
+            long prev = 0;
+            long next = 1;
+            for (int i = 1; i < x; i++)
+            {
+                long sum = prev + next;
+                prev = next;
+                next = sum;
+            }
+            return next;
+           
+        }
+
+        public  string ReverseWords(string str)
+        {
+            if(str==null)
+            {
+                throw new ArgumentNullException("Null Argument");
+            }
+            var reversedWords = string.Join(" ",str.Split(' ')
+          .Select(x => new String(x.Reverse().ToArray())));
+        
+       return reversedWords;
+            
+        }
+    }
+}
